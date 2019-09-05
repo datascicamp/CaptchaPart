0e7e4e27999b02226d97ab85d128869ba92ca2e1
XinyaoTian <leontian1024@gmail.com>
2019-09-05 12:56:48 +0800

    Change font to DejaVu Serif

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ class Config(object):
 
     SECRET_KEY = os.environ.get('WEB_SERVER_SECRET_KEY') or 'abcdef020301abc8c86f'
 
-
     # Identifiers when generating CAPTCHA
     identifiers = 'ABCDEFGHIJKLMNPQRSTUVWXYZ12345789'
