40ce5d5f904a686412c0622d1be87a06bdfafe0b
XinyaoTian <leontian1024@gmail.com>
2019-09-19 14:10:28 +0800

    Add tutorial.

--- app/routes.py
+++ app/routes.py
@@ -6,9 +6,21 @@ from flask import render_template
 @app.route('/usage')
 def usage():
     usages = [
+        {'api_format': '/api/hash-match/captcha', 'method': 'GET',
+         'Warning': 'Default 4 Characters.',
+         'description': 'Get a CAPTCHA code and a matching hash code in Response.headers.CAPTCHA'
+                        ' and Response.headers.Hash-Code field. Then you can use'
+                        ' /api/captcha/hash-code/<string:hash_code> to get captcha code by hash code.'},
+        {'api_format': '/api/captcha/hash-code/<string:hash_code>', 'method': 'GET',
+         'Warning': 'Default 4 Characters.',
+         'description': 'Get a CAPTCHA image in HTTP.content and its code in Response.headers.CAPTCHA field'
+                        ' through your hash code which already given in /api/hash-match/captcha.'},
+        {'api_format': '/api/captcha/hash-code/<int:seconds>', 'method': 'DELETE',
+         'Warning': 'Delete captcha-hash matching record.',
+         'description': 'Delete expired captcha-hash matching record by seconds you given.'},
         {'api_format': '/api/captcha', 'method': 'GET',
          'Warning': 'Generate 5 character in an image.',
-         'description': 'Get a CAPTCHA image in HTTP.content and its code in HTTP.headers.CAPTCHA field.'},
+         'description': 'Get a CAPTCHA image in HTTP.content and its code in Response.headers.CAPTCHA field.'},
         {'api_format': '/api/captcha/<string:captcha_code>', 'method': 'GET',
          'Warning': 'At Most 4 Characters!',
          'description': 'Get a CAPTCHA image based on string you given in Get method.'},
